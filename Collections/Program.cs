a2e0cb527fff2ca1fd29f11e4cb9b4e8a24a741c
Oskar <oskar.legner@zsk.poznan.pl>
2024-11-14 13:39:20 +0100

    Add collections examples

---
+++ Collections/Program.cs
@@ -0,0 +1,53 @@
+﻿// Array
+
+using System.Collections;
+
+int[] intsArr = new int[5]; 
+intsArr[0] = 1;
+intsArr[3] = 2;
+
+for (int i = 0; i < intsArr.Length; i++)
+{
+    intsArr[i] = i * 5;
+}
+
+foreach (int item in intsArr)
+{
+    Console.WriteLine(item);
+}
+
+
+List<int> intsList = new List<int>();
+
+intsList.Add(50);
+intsList.Add(200);
+intsList.Add(4000);
+intsList.Add(4000);
+
+// List
+
+Console.WriteLine(" --- ");
+for (int i = 0;i < intsList.Count;i++)
+{
+    Console.WriteLine(intsList[i]);
+}
+intsList.Remove(4000);
+intsList.RemoveAt(0);
+intsList.RemoveAll(el => el > 100);
+intsList.Insert(0, 99);
+
+Console.WriteLine(" --- ");
+for (int i = 0; i < intsList.Count; i++)
+{
+    Console.WriteLine(intsList[i]);
+}
+
+
+
+// ArrayList
+
+ArrayList x = new ArrayList();
+x.Add(1);
+x.Add("3");
+
+
