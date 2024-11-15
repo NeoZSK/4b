5a887fd60dd7d63cae1785927d480d389dc78839
Oskar <oskar.legner@zsk.poznan.pl>
2024-11-15 09:45:09 +0100

    Add example of cursor moving

---
+++ Cursor/Cursor/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Cursor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pos = 0;
+            while (true)
+            {
+                for (int i = 0; i < Console.WindowWidth; i++) 
+                    Console.Write("_");
+
+                Console.SetCursorPosition(pos, 0);
+                Console.Write("*");
+                
+                pos = (++pos % Console.WindowWidth);
+                Thread.Sleep(1000);
+                Console.Clear();
+
+            }
+
+
+
+
+        }
+    }
+}
