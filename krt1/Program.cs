8c170a8297945cf95553d3d2ebdf32b6f2363db6
Oskar <oskar.legner@zsk.poznan.pl>
2024-11-15 09:44:21 +0100

    Add example of solution for first kartkowka

---
+++ krt1/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("somethign");
+
+
+const int WIDTH = 10;
+const int HEIGHT = 5;
+
+int x = 3, y = 3;
+
+while (true)
+{
+
+    Console.Clear();
+    // Draw map
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        {
+            for (int j = 0; j < WIDTH; j++)
+            {
+
+                if (j == x && i == y)
+                {
+                    Console.Write("*");
+                }
+
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    ConsoleKeyInfo k = Console.ReadKey();
+
+    // move
+    switch (k.Key)
+    {
+        case ConsoleKey.DownArrow:
+                y = ++y % WIDTH; 
+
+            break;
+
+        case ConsoleKey.UpArrow:
+            y--; break;
+    }
+}
