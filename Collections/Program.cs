6fd5cceb3b48e964b48839e2f0e9804f8d6d4cf6
Oskar <oskar.legner@zsk.poznan.pl>
2024-11-26 14:47:22 +0100

    Add 2dim arrays example

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,64 +1,89 @@
-﻿
+﻿int[,] intsArr = new int[3, 5];
+int[,] intsArr2 = {
+    { 0, 1, 2, 2, 2 },
+    { 1, 2, 3, 3, 3 },
+    { 3, 4, 5, 3, 3 },
+};
 
-
-
-
-
-
-
-
-
-
-void arrays()
+for (int i = 0; i < intsArr2.GetLength(0); i++)
 {
-    int[] intsArr = new int[5];
-    intsArr[0] = 1;
-    intsArr[3] = 2;
-
-    for (int i = 0; i < intsArr.Length; i++)
+    for (int j = 0; j < intsArr2.GetLength(1); j++)
     {
-        intsArr[i] = i * 5;
+        Console.Write(intsArr2[i, j]);
+        Console.Write(" ");
     }
-
-    foreach (int item in intsArr)
-    {
-        Console.WriteLine(item);
-    }
-
+    Console.WriteLine();
 }
-void lists()
+
+Console.WriteLine();
+
+for (int i = 0; i < intsArr2.GetLength(1); i++)
 {
-    List<int> intsList = new List<int>();
-
-    intsList.Add(50);
-    intsList.Add(200);
-    intsList.Add(4000);
-    intsList.Add(4000);
-
-
-    for (int i = 0; i < intsList.Count; i++)
+    for (int j = 0; j < intsArr2.GetLength(0); j++)
     {
-        Console.WriteLine(intsList[i]);
+        Console.Write(intsArr2[j, i]);
+        Console.Write(" ");
     }
-    intsList.Remove(4000);
-    intsList.RemoveAt(0);
-    intsList.RemoveAll(el => el > 100);
-    intsList.Insert(0, 99);
-
-    Console.WriteLine(" --- ");
-    for (int i = 0; i < intsList.Count; i++)
-    {
-        Console.WriteLine(intsList[i]);
-    }
-
-
+    Console.WriteLine();
 }
-void arrayLists()
-{
-    ArrayList x = new ArrayList();
-    x.Add(1);
-    x.Add("3");
-}
+
+
+
+
+
+
+
+
+//void arrays()
+//{
+//    int[] intsArr = new int[5];
+//    intsArr[0] = 1;
+//    intsArr[3] = 2;
+
+//    for (int i = 0; i < intsArr.Length; i++)
+//    {
+//        intsArr[i] = i * 5;
+//    }
+
+//    foreach (int item in intsArr)
+//    {
+//        Console.WriteLine(item);
+//    }
+
+//}
+//void lists()
+//{
+//    List<int> intsList = new List<int>();
+
+//    intsList.Add(50);
+//    intsList.Add(200);
+//    intsList.Add(4000);
+//    intsList.Add(4000);
+
+
+//    for (int i = 0; i < intsList.Count; i++)
+//    {
+//        Console.WriteLine(intsList[i]);
+//    }
+//    intsList.Remove(4000);
+//    intsList.RemoveAt(0);
+//    intsList.RemoveAll(el => el > 100);
+//    intsList.Insert(0, 99);
+
+//    Console.WriteLine(" --- ");
+//    for (int i = 0; i < intsList.Count; i++)
+//    {
+//        Console.WriteLine(intsList[i]);
+//    }
+
+
+//}
+//void arrayLists()
+//{
+//    ArrayList x = new ArrayList();
+//    x.Add(1);
+//    x.Add("3");
+//}
 
 
 
